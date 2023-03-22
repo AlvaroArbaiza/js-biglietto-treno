d3d6a165bf0a108d44a8bca1a1b187d056698204
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-22 17:49:16 +0100

    Sviluppo Esercizio

---
+++ assets/javascript/main.js
@@ -0,0 +1,42 @@
+/*
+
+- Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+- Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+    - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    - va applicato uno sconto del 20% per i minorenni
+    - va applicato uno sconto del 40% per gli over 65.
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+*/
+
+
+function myFunction() { 
+
+    // chilometri
+    let chilometri = document.getElementById("chilometri").value;
+
+    // età
+    let eta = document.getElementById("eta").value;
+
+    // tariffa in base al numero di chilometri
+    const tariffa = 0.21;
+
+    // prezzo totale
+    let prezzoTotale = tariffa * chilometri;
+
+    // sconto del 20%
+    if (eta < 18) {
+        prezzoTotale = prezzoTotale * 0.2;
+    }
+
+    // sconto del 40%
+    if (eta > 65) {
+        prezzoTotale = prezzoTotale * 0.4;
+    }
+
+    document.getElementById("prezzoFinale").innerHTML = "Il prezzo del biglietto è: " + prezzoTotale.toFixed(2);
+
+    console.log(prezzoTotale);
+
+    console.log("ciao");
+}
